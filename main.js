228491a70c5e9c765a2b18f6680aadb0795495dd
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-10-10 22:09:16 -0500

    finsihed

--- main.js
+++ main.js
@@ -4,54 +4,77 @@ const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-// An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// An object that represents the three stacks of Towers of Hanoi;
+// * each key is an array of Numbers:
+// * A is the far-left,
+// * B is the middle,
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens:
+// * 4 is the largest,
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
-  c: []
+  c: [],
 };
 
 // Start here. What is this function doing?
 const printStacks = () => {
-  console.log("a: " + stacks.a);
-  console.log("b: " + stacks.b);
-  console.log("c: " + stacks.c);
-}
+  console.log('a: ' + stacks.a);
+  console.log('b: ' + stacks.b);
+  console.log('c: ' + stacks.c);
+};
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
-
-}
+const movePiece = (startStack, endStack) => {
+  if (isLegal(startStack, endStack) === true) {
+    let topItem = stacks[startStack].pop();
+    stacks[endStack].push(topItem);
+  }
+};
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
+const isLegal = (startStack, endStack) => {
+  const lastIndex = stacks[startStack].length - 1;
+  const startArray = stacks[startStack];
+  const start = startArray[lastIndex];
+  const lastEnd = stacks[endStack].length - 1;
+  const endArray = stacks[endStack];
+  const end = endArray[lastEnd];
 
-}
+  if (start < end) {
+    return true;
+  } else if (start > end) {
+    return false;
+  } else if (end === undefined) {
+    return true;
+  } else if (start === undefined) {
+    return false;
+  }
+};
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
-
-}
+  if (stacks.c.length === 4 || stacks.b.length === 4) {
+    console.log('player has won');
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
+  movePiece(startStack, endStack);
 
-}
+  checkForWin(startStack, endStack);
+};
 
 const getPrompt = () => {
   printStacks();
@@ -61,12 +84,11 @@ const getPrompt = () => {
       getPrompt();
     });
   });
-}
+};
 
 // Tests
 
 if (typeof describe === 'function') {
-
   describe('#towersOfHanoi()', () => {
     it('should be able to move a block', () => {
       towersOfHanoi('a', 'b');
@@ -79,7 +101,7 @@ if (typeof describe === 'function') {
       stacks = {
         a: [4, 3, 2],
         b: [1],
-        c: []
+        c: [],
       };
       assert.equal(isLegal('a', 'b'), false);
     });
@@ -87,7 +109,7 @@ if (typeof describe === 'function') {
       stacks = {
         a: [4, 3, 2, 1],
         b: [],
-        c: []
+        c: [],
       };
       assert.equal(isLegal('a', 'c'), true);
     });
@@ -100,9 +122,6 @@ if (typeof describe === 'function') {
       assert.equal(checkForWin(), false);
     });
   });
-
 } else {
-
   getPrompt();
-
 }
